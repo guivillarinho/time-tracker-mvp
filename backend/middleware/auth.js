fc98ae0f6260013f6b47ea5613534bf84c56f411
guilherme oliveira villarinho <guivillarinho15@gmail.com>
2025-10-16 13:31:07 -0300

    feat: criação do middleware de autenticação

---
+++ backend/middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+// Middleware para verificar o token JWT em requisições
+module.exports = function (req, res, next) {
+    // Obter o token do header da requisição
+    const token = req.header('x-auth-token');
+
+    // Checar se o token não existe
+    if (!token) {
+        return res.status(401).json({ msg: 'Nenhum token, autorização negada' });
+    }
+
+    // Verificar o token
+    try {
+        // Decodifica o token usando a chave secreta do .env
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        // Adiciona o usuário decodificado ao objeto de requisição (req.user)
+        req.user = decoded.user;
+        next(); // Continua para a próxima função da rota
+    } catch (err) {
+        res.status(401).json({ msg: 'Token não é válido' });
+    }
+};
